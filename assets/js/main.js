bed809f23c8b2f66dfd70f08e02754af293b7de4
Nikhil <nikhilmaurya10@gmail.com>
2017-02-27 03:40:37 +0530

    Added landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-main.js
+(function(){
+
+	$(".video-modal").hide();
+
+
+
+	$("#play").on('click', function(){
+
+		$(".video-modal").fadeIn();
+	});
+
+	$("#esc").on('click', function(){
+
+		$(".video-modal").fadeOut();
+	});
+
+
+})();
